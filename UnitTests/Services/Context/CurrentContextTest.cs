b5d418cc5297e878e7f3fcbe36e8c973e8ae486c
Mathieu VIALES <mathieu@viales.fr>
2020-11-10 10:30:04 +0100

    Test current context

---
+++ UnitTests/Services/Context/CurrentContextTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Desktop.Services.Context;
+using Xunit;
+
+namespace UnitTests.Services.Context
+{
+    public class CurrentContextTest
+    {
+        #region BOILERPLATE
+
+        private static CurrentContext Construct()
+        {
+            var state = new CurrentContext();
+            return state;
+        }
+
+        #endregion
+
+        #region SET
+
+        [Fact]
+        public async Task Set_changes_get()
+        {
+            var state = Construct();
+            state.Set("current");
+            Assert.Equal("current",await state.Get());
+        }
+
+        #endregion
+    }
+}
