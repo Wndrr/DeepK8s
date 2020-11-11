3d2a0c5f548466cffec55238a4481e49f59b06ef
Mathieu VIALES <mathieu@viales.fr>
2020-11-11 10:11:09 +0100

    38 Merge EntityKindReference components (#65)
    * #38 - Merge entity kinds into one
    
    * Remove icons
    
    * Fix ArgumentNullException unitTests

--- Desktop/Services/EntityReferenceUrlBuilder.cs
+++ Desktop/Services/EntityReferenceUrlBuilder.cs
@@ -4,14 +4,12 @@ namespace Desktop.Services
 {
     public class EntityReferenceUrlBuilder
     {
-        public Uri BuildReferenceUri(string pageName, string entityName, string entityNamespace = null)
+        public Uri BuildEntityReferenceUri(string entityKind, string entityName, string? entityNamespace = null)
         {
-            if(string.IsNullOrEmpty(pageName))
-                throw new ArgumentException("Can't be null or empty", nameof(pageName));
-            if(string.IsNullOrEmpty(entityName))
-                throw new ArgumentException("Can't be null or empty", nameof(entityName));
+            if(string.IsNullOrEmpty(entityKind)) throw new ArgumentNullException(nameof(entityKind), "Can't be null or empty");
+            if(string.IsNullOrEmpty(entityName)) throw new ArgumentNullException(nameof(entityKind), "Can't be null or empty");
             
-            var entityReferenceUri = entityNamespace == null ? $"/{pageName}/{entityName}" : $"/{entityNamespace}/{pageName}/{entityName}";
+            var entityReferenceUri = entityNamespace == null ? $"/{entityKind}/{entityName}" : $"/{entityNamespace}/{entityKind}/{entityName}";
 
             return new Uri(entityReferenceUri, UriKind.Relative);
         }
